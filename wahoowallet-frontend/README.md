616702b338891704e7aaf5674cf64fa4109d0e0e
Christopher Tran <chrisgktran@gmail.com>
2024-11-08 18:15:16 -0500

    adding front-end stuff

---
+++ wahoowallet-frontend/README.md
@@ -0,0 +1,6 @@
+# Front-end
+helllooo, some steps to get started
+1. git clone https://github.com/Project-Code-UVA/WahooWallet.git
+2. cd wahoowallet-frontend
+3. npm install
+4. npm run dev
---
+++ wahoowallet-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.module.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1> hi :0 </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
