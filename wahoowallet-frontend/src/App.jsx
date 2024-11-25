d82845d321c87c84bd76abec487beaba2c831fc9
edmundjr <edmund.rintis@gmail.com>
2024-11-25 01:48:22 -0500

    Testing Bank Account Page

--- wahoowallet-frontend/src/App.jsx
+++ wahoowallet-frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.module.css'
 import LoginPage from './components/Login/Login.jsx'
+import BankAccountPage from './components/BankAccount/BankAccount.jsx'
 
 function App() {
 
   return (
-    <LoginPage />
+    <BankAccountPage />
+    
   )
 }
 
