651189d7f9a669a38b9e418ffef7aae0e56dd179
lxve23 <issawasteland@gmail.com>
2025-01-07 13:58:22 -0500

    Trailing forward slash fix

--- wahoowallet-frontend/src/auth/ProtectedRoute.jsx
+++ wahoowallet-frontend/src/auth/ProtectedRoute.jsx
@@ -1,12 +1,22 @@
-import { Navigate } from "react-router";
+import { useEffect } from "react";
+import { Navigate, useLocation } from "react-router";
 
 export default function ProtectedRoute({ children }) {
   // Real auth logic needed once DB is setup
   const isLoggedIn = true;
-  return isLoggedIn ? children : <Navigate to='/login' />;
-  /*
-    Needs fix: When not logged in, and filepath is '/' more than one time
-    Eg. '//', '/////', etc, without any other characters,
-    <Navigate /> cannot redirect to '/login'
+
+  /* 
+    Fix for trailing forward slashes in pathname breaking react-router (super niche)
+    '//', '////', etc, get forced to '/'
+    '/url/', '//url', '/url//' are untouched.
   */
+  const location = useLocation();
+  useEffect(() => {
+    const multipleSlashes = location.pathname.match(/^\/{2,}$/);
+    if (multipleSlashes) {
+      window.location.pathname = '/';
+    }
+  }, [location]);
+
+  return isLoggedIn ? children : <Navigate to='/login' />;
 }
