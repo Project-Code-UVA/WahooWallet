ffcf392c18f344ee697b3df81a161545aec320ab
radwnkhn <kradwan257@gmail.com>
2025-04-06 23:52:20 -0400

    changed settings

--- wahoowallet-backend/backend/settings.py
+++ wahoowallet-backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-na%i529)n%hdb4jfrm2hpy@d_!hl_%)cqcj2!h6*mfn&sgw(qw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://wahoowallet.onrender.com']
+ALLOWED_HOSTS = ['wahoowallet.onrender.com', 'localhost', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://wahoowallet.onrender.com'
