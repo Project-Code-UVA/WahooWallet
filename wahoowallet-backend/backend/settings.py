8ca8712171ce9a3f3df9e38d0e1654acb894bce5
radwnkhn <kradwan257@gmail.com>
2025-04-06 23:18:59 -0400

    updated setting.py

--- wahoowallet-backend/backend/settings.py
+++ wahoowallet-backend/backend/settings.py
@@ -17,6 +17,7 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -130,6 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
